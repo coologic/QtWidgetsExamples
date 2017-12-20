86cea6744c1e62a73f97de66882c1280e4d2f433
zhouliang <zhouliangwork@163.com>
2017-12-20 13:21:59 +0800

    add QTextDocumentExample

---
+++ QTextDocumentExample/mainwindow.cpp
@@ -0,0 +1,39 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QTextDocument>
+#include <QTextFrame>
+#include <QTextBlock>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+    QTextDocument* doc = ui->textEdit->document();
+    QTextFrame *root_frame = doc->rootFrame();
+    QTextFrameFormat root_frame_format = root_frame->frameFormat();//创建框架格式
+    root_frame_format.setBorderBrush(Qt::darkBlue);//设置边界颜色
+    root_frame_format.setBorder(5);//设置边界宽度
+    root_frame->setFrameFormat(root_frame_format); //给框架使用格式
+    QTextFrameFormat frame_format;
+    frame_format.setBackground(Qt::darkRed);//设置背景色
+    frame_format.setMargin(10);//设置边距
+    frame_format.setPadding(5);//设置填充
+    frame_format.setBorder(2);//设置边界宽度
+    frame_format.setBorderStyle(
+                QTextFrameFormat::BorderStyle_Solid);//设置边框样式
+    frame_format.setPosition(QTextFrameFormat::FloatRight);//右侧
+    frame_format.setWidth(QTextLength(
+                QTextLength::PercentageLength, 40));//宽度设置
+    QTextCursor cursor = ui->textEdit->textCursor();
+    cursor.insertText("A company");
+    cursor.insertBlock();
+    cursor.insertText("321 City Street");
+    cursor.insertBlock();
+    cursor.insertFrame(frame_format);
+    cursor.insertText("Industry Park");
+    cursor.insertBlock();
+    cursor.insertText("Another country");
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
--- README.md
+++ README.md
@@ -9,7 +9,7 @@ QWidget创建过程的事件顺序
 ## QtCustomTitleBar
 Qt自定义标题栏
 * Blog：[Qt自定义标题栏](http://techieliang.com/2017/11/326/)  
-:![pic](https://github.com/TechieL/QtWidgetsExamples/blob/master/QtCustomTitleBar/pic.png)  
+![pic](https://github.com/TechieL/QtWidgetsExamples/blob/master/QtCustomTitleBar/pic.png)  
 使用方法：  
 直接在主窗口(QMainWindow)调用：  
 TitleBar *titlebar = new TitleBar(this);  
@@ -18,4 +18,10 @@ TitleBar *titlebar = new TitleBar(this);
 ## QCheckboxUsedbyQTreeViewandQTableView
 在QTreeView和QTableView中使用复选框实现三态变化
 * Blog：[QTreeView/QTableView中利用QStandardItem实现复选框三种形态变化](http://techieliang.com/2017/12/729/)  
-:![pic](https://github.com/TechieL/QtWidgetsExamples/blob/master/QCheckboxUsedbyQTreeViewandQTableView/gif.gif)  
+![pic](https://github.com/TechieL/QtWidgetsExamples/blob/master/QCheckboxUsedbyQTreeViewandQTableView/gif.gif)  
+
+## QTextDocumentExample
+Qt富文本编辑器使用
+* Blog：[Qt富文本编辑器QTextDocument](http://techieliang.com/2017/12/726/)  
+![pic](https://github.com/TechieL/MyBlogPictureBackup/blob/master/%E5%9B%BE%E7%89%87/%E6%96%87%E7%AB%A0%E5%9B%BE%E7%89%87/Qt%E5%AF%8C%E6%96%87%E6%9C%AC%E7%BC%96%E8%BE%91%E5%99%A8QTextDocument/1.png)  
+
