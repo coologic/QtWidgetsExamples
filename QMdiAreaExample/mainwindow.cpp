f774b077802aba71079629e4ee5621bc15cbc1e5
zhouliang <zhouliangwork@163.com>
2017-12-20 22:51:49 +0800

    add midarea

---
+++ QMdiAreaExample/mainwindow.cpp
@@ -0,0 +1,63 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMdiSubWindow>
+#include <QList>
+#include <QDebug>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->mdiArea->setTabsMovable(true);
+    ui->mdiArea->setTabShape(QTabWidget::Triangular);
+    //默认不全屏，否则在TabbedView就只显示一个窗口而且没有标题栏
+    ui->mdiArea->setOption(QMdiArea::DontMaximizeSubWindowOnActivation);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked() {
+    QMdiSubWindow *mdi = new QMdiSubWindow;
+    ui->mdiArea->addSubWindow(mdi);
+    //关闭之后不会自动释放空间，需要此函数让其在close后自动释放
+    mdi->setAttribute(Qt::WA_DeleteOnClose);
+    //add以后也不自动显示，需要主动show
+    mdi->show();
+}
+
+void MainWindow::on_pushButton_deletecurrent_clicked() {
+    ui->mdiArea->closeActiveSubWindow();
+}
+
+void MainWindow::on_pushButton_deleteall_clicked() {
+    auto subs = ui->mdiArea->subWindowList();
+    qDebug()<<"before delete sub size:"<<subs.size();
+    ui->mdiArea->closeAllSubWindows();
+    subs = ui->mdiArea->subWindowList();
+    //可以查看在setAttribute是否设置状态下list是否变动
+    //使用以后此处为0，不使用为新建窗口数且内存不会减少
+    qDebug()<<"after delete sub size:"<<subs.size();
+}
+
+void MainWindow::on_pushButton_change1_clicked() {
+    if(ui->mdiArea->viewMode() == QMdiArea::TabbedView)
+        ui->mdiArea->setViewMode(QMdiArea::SubWindowView);
+    else
+        ui->mdiArea->setViewMode(QMdiArea::TabbedView);
+}
+
+void MainWindow::on_pushButton_change2_clicked() {
+    delete ui->mdiArea->layout();
+    QHBoxLayout *mLayout=new QHBoxLayout(ui->mdiArea);
+    auto subs = ui->mdiArea->subWindowList();
+    for(auto sub : subs) {
+        sub->hide();
+        mLayout->addWidget(sub);
+        //不隐藏显示就不能生效，update不管用
+        sub->show();
+    }
+    ui->mdiArea->setLayout(mLayout);
+}
--- README.md
+++ README.md
@@ -25,3 +25,8 @@ Qt富文本编辑器使用
 * Blog：[Qt富文本编辑器QTextDocument](http://techieliang.com/2017/12/726/)  
 ![pic](https://github.com/TechieL/MyBlogPictureBackup/blob/master/%E5%9B%BE%E7%89%87/%E6%96%87%E7%AB%A0%E5%9B%BE%E7%89%87/Qt%E5%AF%8C%E6%96%87%E6%9C%AC%E7%BC%96%E8%BE%91%E5%99%A8QTextDocument/1.png)  
 
+## QMdiAreaExample
+Qt子窗口QMdiArea和QMdiSubWindow的使用
+* Blog：[QMdiArea及QMdiSubWindow实现父子窗口及布局方法](http://techieliang.com/2017/12/756/)  
+![pic](https://github.com/TechieL/MyBlogPictureBackup/blob/master/%E5%9B%BE%E7%89%87/%E6%96%87%E7%AB%A0%E5%9B%BE%E7%89%87/QMdiArea%E5%8F%8AQMdiSubWindow%E5%AE%9E%E7%8E%B0%E7%88%B6%E5%AD%90%E7%AA%97%E5%8F%A3%E5%8F%8A%E5%B8%83%E5%B1%80%E6%96%B9%E6%B3%95/gif.gif)  
+
